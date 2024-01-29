018747544dd7288900c4785aa3d0301c7ca0843d
mindahFranck <mindahnkemeni@gmail.com>
2024-01-29 18:25:12 +0100

    update

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,7 @@
 export const formatPrice = (number) => {
   const newNumber = Intl.NumberFormat("en-US", {
     style: "currency",
-    currency: "USD",
+    currency: "XAF",
   }).format(number / 100);
   return newNumber;
 };
