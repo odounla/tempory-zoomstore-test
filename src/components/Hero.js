11e52a7a87001530119e31567fdd96a57112e4da
OLIVIER DOUNLA <olivierdounla@OLIVIERs-MacBook-Pro.local>
2024-01-29 16:11:57 -0800

    adding the products after setting in itgrafik

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => {
       <article className="content">
         <h1>
           buy your <br />
-          dream accessories and goodies
+          dream accessories
         </h1>
         <p>
           We help you find your dream accesories, or what fit you the most. Zoom
